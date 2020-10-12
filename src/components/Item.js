29a117565c5a42a5b3ef41d05943feb87de004e1
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-10-11 22:00:50 -0400

    item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { MdEdit, MdDelete } from 'react-icons/md';
 
-const Item = () => {
-  return <div>items</div>;
+const Item = ({ id, charge, amount }) => {
+  // const { id, charge, amount } = expense;
+  return (
+    <li>
+      <div className='info'>
+        <span className='expense'>{charge}</span>
+        <span className='amount'>{amount}</span>
+      </div>
+      <div>
+        <button className='edit-btn' aria-label='Edit button'>
+          <MdEdit />
+        </button>
+        <button className='clear-btn' aria-label='Delete button'>
+          <MdDelete />
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default Item;
