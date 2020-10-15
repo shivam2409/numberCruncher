f9573f367a538c6a72bd1f9851ca13e9af9583d6
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-10-15 18:29:28 -0400

    handle submit

--- src/App.js
+++ src/App.js
@@ -39,6 +39,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(charge, amount);
+    if (charge !== '' && amount > 0) {
+      const singleExpense = { id: uuid(), charge, amount };
+      setExpenses([...expenses, singleExpense]);
+      setCharge('');
+      setAmount('');
+    } else {
+    }
   };
   return (
     <div>
@@ -59,7 +67,7 @@ function App() {
         <span className='total'>
           $
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
